91e34ab2b6274d0ffea5321160f4dd41059599fd
Peter <dakcsi@gmail.com>
2016-10-16 03:12:23 +0200

    hello world

--- gomoku.js
+++ gomoku.js
@@ -2,4 +2,4 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(9001, '64.137.255.190');
+}).listen(8080, '64.137.255.190');
