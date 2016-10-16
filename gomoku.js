ec55835c8163112f798b57c9a24c3cbc24d0401c
Peter <dakcsi@gmail.com>
2016-10-16 02:28:47 +0200

    hello world

--- gomoku.js
+++ gomoku.js
@@ -3,14 +3,3 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
 }).listen(9001, '64.137.255.190');
-
-
-
-var http = require('http');
-var exec = require('child_process').exec;
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  exec('sudo bash hook.sh');
-  console.log('git repo synced...');
-  res.end(''git repo synced...'');
-}).listen(9001, '64.137.255.190');
